f22e2b5d83fc63526fd0ca7ed3d5d5dcd85acca1
JL102 <jordanlees@mailbox.org>
2024-03-01 17:23:50 -0500

    Scripting HW

--- Assets/Imported stuff/Characters/PlayerController.cs
+++ Assets/Imported stuff/Characters/PlayerController.cs
@@ -14,14 +14,20 @@ public class PlayerController : MonoBehaviour
     bool moving;
     Animator animator;
     GameObject body;
+    GameObject camera;
+
+    int floor, breakable;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
         body = GameObject.Find("Body");
+        camera = GameObject.Find("Main Camera");
         animator = body.GetComponent<Animator>();
         moving = false;
+        floor = LayerMask.NameToLayer("Floor");
+        breakable = LayerMask.NameToLayer("Breakable");
     }
 
     void FixedUpdate()
@@ -49,6 +55,23 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(new Vector3(0f, jumpForce * forceMultiple, 0f));
             animator.SetBool("inAir", true);
             animator.SetTrigger("startJump");
+        }
+        camera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, camera.transform.position.z);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == floor)
+        {
+            animator.SetBool("inAir", false);
+        }
+        else if (other.gameObject.layer == breakable)
+        {
+            // If below the other breakable object, then destroy it
+            if (this.transform.position.y < other.gameObject.transform.position.y)
+            {
+                Destroy(other.gameObject);
+            }
         }
     }
 }
